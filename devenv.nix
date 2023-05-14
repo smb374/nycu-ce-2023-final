f10f666f588e1f8e156ddcf7b906f300b0348f70
Po-Yeh Chen <poyehchen@cs.nctu.edu.tw>
2023-05-14 23:15:51 +0800

    Setup development environment.

---
+++ devenv.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [ git gmp ];
+
+  # https://devenv.sh/scripts/
+
+  enterShell = ''
+    git --version
+  '';
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+  languages.python = {
+    enable = true;
+    package = pkgs.python311.withPackages (ps: with ps;
+      [
+        gmpy2
+      ]);
+    venv.enable = true;
+  };
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
